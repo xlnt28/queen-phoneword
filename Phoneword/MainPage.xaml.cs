e00e1bfbf081f68f9105b93c7b86375a48576ff9
Dexter Facelo <Dexter101306@gmail.com>
2026-04-26 13:21:40 +0800

    initial commit - base

---
+++ Phoneword/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+    string? translatedNumber;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlertAsync(
+            "Dial a Number",
+            "Would you like to call " + translatedNumber + "?",
+            "Yes",
+            "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported &&
+                    !string.IsNullOrWhiteSpace(translatedNumber))
+                    PhoneDialer.Default.Open(translatedNumber);
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlertAsync("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                // Other error has occurred.
+
+                await DisplayAlertAsync("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+}
